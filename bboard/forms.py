79b00182d35cf6329f1111200579d82ee6cee22e
Aibatyr <aibatyr111@gmail.com>
2025-02-18 21:32:31 +0500

    lesson26

--- bboard/forms.py
+++ bboard/forms.py
@@ -1,10 +1,13 @@
 from dataclasses import fields
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm, modelform_factory, DecimalField
 from django.forms.widgets import Select
 
 from bboard.models import Bb, Rubric
+
+from django.core import validators
 
 
 #1
@@ -31,12 +34,37 @@ from bboard.models import Bb, Rubric
 
 #3
 class BbForm(ModelForm):
-    title = forms.CharField(label="Название товара")
+    title = forms.CharField(label="Название товара",
+                            validators=[validators.RegexValidator(regex='^.{4,}$')],
+                            error_messages={'invalid': 'Сдишком короткое название'})
     content = forms.CharField(label="Описание", widget=forms.Textarea())
     price = forms.DecimalField(label='Цена',  decimal_places=2)
     rubric = forms.ModelChoiceField(queryset=Rubric.objects.all(),
                                     label='Рубрика', help_text='Не забудьте выбрать рубрику',
                                     widget = forms.widgets.Select(attrs={'size':8}))
+
+
+    def clean_title(self):
+        val = self.cleaned_data['title']
+        if val == 'Прошлогодний снег':
+            raise ValidationError('К продаже не допуск')
+        return val
+
+    def clean(self):
+        super().clean()
+        errors = {}
+        if not self.cleaned_data['content']:
+            errors['content'] = ValidationError(
+                'кажите описание товара'
+            )
+
+        if not self.cleaned_data['price'] < 0:
+            errors['price'] = ValidationError(
+                'price>0'
+            )
+        if errors:
+            raise ValidationError(errors)
+
 
     class Meta:
         model = Bb
